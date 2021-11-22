e23290b9904b6b059e8dd9275006d0c883999691
Nonopichy <68911691+Nonopichy@users.noreply.github.com>
2021-11-22 11:59:07 -0300

    Update Recipe.java

--- src/main/java/com/nonopichy/customrecipe/Recipe.java
+++ src/main/java/com/nonopichy/customrecipe/Recipe.java
@@ -1,17 +1,15 @@
 package com.nonopichy.customrecipe;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.bukkit.inventory.ItemStack;
 
 @Data
-@AllArgsConstructor
 public class Recipe {
 
     private MatrixItem[] recipe;
     private ItemStack result;
     private boolean loose;
-    
+
     public void setRecipe(MatrixItem... m) {
         recipe = m;
     }
